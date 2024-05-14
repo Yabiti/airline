1133cbbc8485bea130a3f61b173ce42cd181cde4
Yabiti <yabetseyohannes@gmail.com>
2024-05-14 14:29:39 +0300

    .

--- airline/flight/views.py
+++ airline/flight/views.py
@@ -1,3 +1,4 @@
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -7,4 +8,6 @@ def home(request):
     return render(request, 'flight/home.html')
 
 def form(request):
+    if request.method == "POST":
+
     return render(request, 'flight/form.html')
