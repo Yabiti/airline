6f0485df66cc24761c39f40b14424c74ffb8d0e1
Yabiti <yabetseyohannes@gmail.com>
2024-05-14 16:11:13 +0300

    .

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": 'django.db.backends.mysql',
+        'NAME': 'Flight',
+        "HOST": '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        "PASSWORD": 'root'
     }
 }
 
