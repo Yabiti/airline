072c43dec648781038afc6433cae675956690faa
Yabiti <yabetseyohannes@gmail.com>
2024-05-15 12:28:12 +0300

    .

--- airline/flight/models.py
+++ airline/flight/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 
 # Create your models here.
+class Flight(models.Model):
+    FirstName = models.CharField(max_length=15)
+    LastName = models.CharField(max_length=15)
+
+    
