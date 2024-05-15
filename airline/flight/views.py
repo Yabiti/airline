59508a3cbe4c8e0f2359a2d965aa0ecdbb3a8fbf
Yabiti <yabetseyohannes@gmail.com>
2024-05-15 13:14:57 +0300

    .

--- airline/flight/views.py
+++ airline/flight/views.py
@@ -12,3 +12,6 @@ def form(request):
 def insertuser(request):
     FirstName = request.post['first name']
     LastName = request.post['last name']
+    x = Flight(FirstName=FirstName,LastName=LastName)
+    x.save()
+    return render(request, 'flight/home.html')
