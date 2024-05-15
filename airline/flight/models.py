4919d997185b6b0a6bf518d2c62c8cde093d4fba
Yabiti <yabetseyohannes@gmail.com>
2024-05-15 12:40:01 +0300

    .

--- airline/flight/models.py
+++ airline/flight/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 # Create your models here.
 class Flight(models.Model):
-    duration = models.CharField(max_length=64)
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=55)
     FirstName = models.CharField(max_length=15, blank=False, null=True)
     LastName = models.CharField(max_length=15, blank=False, null=True)
 
