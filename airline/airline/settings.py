67ce3823a768d7affa1bd0028a5df6350cca9a93
Yabiti <yabetseyohannes@gmail.com>
2024-05-15 11:03:20 +0300

    .

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -76,15 +76,16 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": 'django.db.backends.mysql',
-        'NAME': 'Flight',
-        "HOST": '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        "PASSWORD": 'root'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE':
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
