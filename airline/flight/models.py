b7bcc1bdf55e25de7a7f74ec4256b6426fb1ae73
Yabiti <yabetseyohannes@gmail.com>
2024-05-24 10:03:21 +0300

    .

--- airline/flight/models.py
+++ airline/flight/models.py
@@ -6,13 +6,22 @@ from django.db import models
 class Flight(models.Model):
     origin = models.CharField(max_length=64, blank=False, null=True)
     destination = models.CharField(max_length=55, blank=False, null=True)
+
+class passenger(models.Model):
     FirstName = models.CharField(max_length=15, blank=False, null=True)
     LastName = models.CharField(max_length=15, blank=False, null=True)
 
     def __str__(self):
         return f"{self.FirstName}: {self.LastName}: {self.origin} to {self.destination}"
+    
     def insertuser(self):
         return self.origin != self.destination
 
     class Meta:
         db_table = "Flight"
+
+class passenger(models.Model):
+    FirstName = models.CharField(max_length=15, blank=False, null=True)
+    LastName = models.CharField(max_length=15, blank=False, null=True)
+
+    
