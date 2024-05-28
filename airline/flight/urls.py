f08f48af1eb80199a674b60179df4394ab07bbbe
Yabiti <yabetseyohannes@gmail.com>
2024-05-28 14:23:10 +0300

    .

--- airline/flight/urls.py
+++ airline/flight/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.home, name="home"),
+    path("lalibella", views.lalibella, name="lalibella"),
     path("form", views.form, name="form"),
-    path("insertuser", views.insertuser, name="insertuser")
+    path("insertuser", views.insertuser, name="insertuser"),
 ]
