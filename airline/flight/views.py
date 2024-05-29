210d8fd5884a5e4e1d2100e053851dfd55e5a274
Yabiti <yabetseyohannes@gmail.com>
2024-05-29 17:39:46 +0300

    .

--- airline/flight/views.py
+++ airline/flight/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 from .models import Flight
 # Create your views here.
 
+# creating home page
 def home(request):
     return render(request, 'flight/home.html')
 
+# creating form
 def form(request):
     return render(request, 'flight/form.html')
 
+# userinput
 def insertuser(request):
     FirstName = request.post['first name']
     LastName = request.post['last name']
